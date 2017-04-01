b2d620e670704e76ca8bde2619dbe2a48fa7f162
U-VLAB\user-lab01 <user-lab01@LAB-01-56.vlab.bmstu.ru>
2017-04-01 11:51:12 +0300

    a

--- lab_02_02/gcov/1/main.c
+++ lab_02_02/gcov/1/main.c
@@ -5,7 +5,7 @@ int Fibonacci(int number)
     int temp = 0; // a
     int tmp = 1; // b
     // Вычисление:
-    for(int i=1; i<number; i++)
+    for(int i = 0; i < number; i++)
     {
         temp += tmp; // a + b = c
         tmp = temp - tmp; // c - b = a
@@ -19,12 +19,12 @@ int main(void)
     // Ввод данных.
     int number, temp;
     printf("Number Fibonacci: ");
-    setbuf(stdout,NULL);
+	setbuf(stdout,NULL);
     if (scanf("%d", &number))
     {
         // Вывод данных.
         temp = Fibonacci(number);
-        printf("Fibonacci: %d\n", temp);
+        printf("Fibonacci: %d", temp);
     }
     else
         printf("Error!");
--- lab_02_02/gcov/2/main.c
+++ lab_02_02/gcov/2/main.c
@@ -3,8 +3,8 @@
 
 int string_hello(char string , float *p, float *z)
 {
-    setbuf(stdout,NULL);
     printf("Input triangle coordinates(x, y) point %c: ", string);
+	setbuf(stdout,NULL);
     if (scanf("%f %f", &*p, &*z) == 2)
         return 1;
     else
@@ -26,25 +26,26 @@ int main(void)
             if (string_hello('C', &x_c, &y_c))
             {
                 float AB_x = x_b - x_a, AB_y = y_b - y_a;
-                float AO_x = x_o - x_a, AO_y = y_o - y_a;
-                float AC_x = x_c - x_a, AC_y = y_c - y_a;
+                float CA_x = x_a - x_c, CA_y = y_a - y_c;
                 float BC_x = x_c - x_b, BC_y = y_c - y_b;
-                if (check_triagle(AB_x, AB_y, AC_x, AC_y))
+                if (check_triagle(AB_x, AB_y, CA_x, CA_y))
                 {
                     printf("Input coordinates(x, y) point O: ");
                     if (scanf("%f %f", &x_o, &y_o))
                     {
                         int s1,s2,s3;
-                        s1 = check_location(AB_x, AB_y, AO_x, AO_y);
-                        s2 = check_location(AC_x, AC_y, AO_x, AO_y);
-                        s3 = check_location(BC_x, BC_y, x_o - x_b, y_o - y_b);
+                        s1 = check_location(AB_x, AB_y, x_o - x_a, y_o - y_a);
+                        s2 = check_location(BC_x, BC_y, x_o - x_b, y_o - y_b);
+                        s3 = check_location(CA_x, CA_y, x_o - x_c, y_o - y_c);
+                        //printf("[DBG] %d %d %d\n", s1, s2, s3);
 
-                        if ((s1+s2+s3)/3 == 1)
+                        if ((s1 < 0 && s2 < 0 && s3 < 0) ||
+                                (s1 > 0 && s2 > 0 && s3 > 0))
                             printf("Point in triagle");
-                        else if ((s1+s2+s3)/3 > 1)
-                            printf("Point outside triagle");
-                        else
+                        else if (s1 == 0 || s2 == 0 || s3 == 0)
                             printf("Point on triagle");
+                        else
+                            printf("Point outside triagle");
                     }
                     else
                         printf("There is no such triagle!");
@@ -70,10 +71,12 @@ int check_triagle(float x_1, float y_1, float x_2, float y_2)
 
 int check_location(float x_1, float y_1, float x_2, float y_2)
 {
+    //printf("[DBG] {%f, %f} {%f, %f}\n", x_1, y_1, x_2, y_2);
+    //printf("[DBG] %f\n", (x_1*y_2 - x_2*y_1));
     if ((x_1*y_2 - x_2*y_1) > 0)
-        return 3;
-    else if ((x_1*y_2 - x_2*y_1) < 0)
         return 1;
+    else if ((x_1*y_2 - x_2*y_1) < 0)
+        return -1;
     else
         return 0;
 }
