0c7c99c42a148211409f9b9699d850edbddece3b
U-VLAB\user-lab01 <user-lab01@LAB-01-04.vlab.bmstu.ru>
2017-05-20 11:53:39 +0300

    lab05 fix

--- lab_05_02/main.c
+++ lab_05_02/main.c
@@ -4,58 +4,69 @@
 #include <errno.h>
 #define MAXLENGTH 100
 #define OK 0
-#define ERROR -1
+#define ERROR_INPUT_FILE -1
+#define ERROR_FOUND_FILE -2
+#define ERROR -3
 
 int usage(void);
 
-int input(FILE*, int*);
+int input(FILE*, int*, int*);
 
-void result(int*, int, long long int*);
+void result(int*, int*, long long int*);
 
 int main(int argc, char** argv)
 {
     FILE *f;
-    f = fopen(*(argv+1), "r");
+    char** link_argv = argv;
+    link_argv++;
+    f = fopen(*link_argv, "r");
     int a[MAXLENGTH];
-    int len;
     //
     if (argc != 2)
         return usage();
     if (!f)
     {
-        fprintf(stderr, "File %s not found! %s!\n", argv[1], strerror(errno));
+        fprintf(stderr, "File %s not found! %s!\n", link_argv, strerror(errno));
         fclose(f);
-        return ERROR;
+        return ERROR_FOUND_FILE;
+    }
+    int rc = OK;
+    //
+    int len = input(f, a, &rc);
+    if (rc)
+        return rc;
+    else
+    {
+        int *pa = a, *pb = a;
+        int i = 0;
+        for (; i < len; ++pb, ++i);
+        //printf("%d %d\n", pb, *pb);
+        long long int res;
+        result(pa, pb, &res);
+        printf("result is %lld", res);
     }
     //
-    len = input(f, a);
-    //printf("%d", len);
-    long long int res;
-    result(a, len, &res);
-    printf("result is %lld", res);
-    //
-    return OK;
+    return rc;
 }
 
 int usage(void)
 {
     printf("example.exe <name file>\n");
-    return ERROR;
+    return ERROR_INPUT_FILE;
 }
 
-int input(FILE*f, int* pa)
+int input(FILE*f, int* pa, int* rc)
 {
-    int len = -1, i = 0;
+    int i = 0;
     //
-    //printf("1# %d\n", len);
-    for (; (fscanf(f, "%d", (pa+i)) == 1); ++i)
+    for (; (fscanf(f, "%d", pa) == 1); ++pa, ++i)
     {
         if (i < MAXLENGTH)
         {
-            //printf("[a]%d\n", *(pa+i));
-            if (*(pa+i) < 0)
+            if (*(pa) < 0)
             {
-                len = i+1; break;
+                //printf("%d %d\n", pa, *pa);
+                break;
             }
         }
         else
@@ -63,28 +74,35 @@ int input(FILE*f, int* pa)
             printf("More than 100 items\n");break;
         }
     }
+    //printf("%d %d\n", pa-1, *(pa-1));
     fclose(f);
-    //printf("2# %d\n", len);
-    if (len == -1)
-        len = (MAXLENGTH - 1);
-    //printf("3# %d\n", len);
-    //
-    return len;
+    if (!i)
+    {
+        *rc = ERROR;
+        return i;
+    }
+    else
+    {
+        if (i < 100)
+            return i;
+        else
+            return i-1;
+    }
 }
 
-void result(int* pa, int len, long long int* sum)
+void result(int* pa, int* pb, long long int* sum)
 {
-    //printf("\n");
     *sum = 0;
     int i = 0;
     long long int squre = 1;
     //
-    for (; i < len; ++i)
+    while(pa <= pb)
     {
-        squre *= *(pa+i);
-        //printf("%d\n", *(pa+i));
+        //printf("%d",*pa);
+        squre *= *pa;
         //printf("\n[dbg] %lld\n", squre);
         *sum += squre;
         //printf("[dbg2] %lld\n", *sum);
+        pa++;
     }
 }
