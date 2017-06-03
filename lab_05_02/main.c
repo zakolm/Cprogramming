214a014c0aa847c376e74bdba2b5471ef4cb65c9
ZakolesnikM <zakolesnik.m@gmail.com>
2017-06-03 10:34:58 +0300

    fix

--- lab_05_02/main.c
+++ lab_05_02/main.c
@@ -6,103 +6,67 @@
 #define OK 0
 #define ERROR_INPUT_FILE -1
 #define ERROR_FOUND_FILE -2
-#define ERROR -3
+#define ERROR_ELEMENTS -3
 
-int usage(void);
-
-int input(FILE*, int*, int*);
-
-void result(int*, int*, long long int*);
+void usage(void);
+void input(FILE*, int*, int*, long long int*);
 
 int main(int argc, char** argv)
 {
     FILE *f;
-    char** link_argv = argv;
-    link_argv++;
-    f = fopen(*link_argv, "r");
-    int a[MAXLENGTH];
-    //
-    if (argc != 2)
-        return usage();
-    if (!f)
-    {
-        fprintf(stderr, "File %s not found! %s!\n", link_argv, strerror(errno));
-        fclose(f);
-        return ERROR_FOUND_FILE;
-    }
     int rc = OK;
-    //
-    int len = input(f, a, &rc);
-    if (rc)
-        return rc;
+    if (argc != 2)
+        usage();
     else
     {
-        int *pa = a, *pb = a;
-        int i = 0;
-        for (; i < len; ++pb, ++i);
-        //printf("%d %d\n", pb, *pb);
-        long long int res;
-        result(pa, pb, &res);
-        printf("result is %lld", res);
+        char** link_argv = argv;
+        link_argv++;
+        f = fopen(*link_argv, "r");
+        int a[MAXLENGTH];
+        //
+        if (!f)
+        {
+            fprintf(stderr, "File %s not found! %s!\n", link_argv, strerror(errno));
+            fclose(f);
+            rc = ERROR_FOUND_FILE;
+        }
+        //
+        long long int res = 0;
+        input(f, a, &rc, &res);
+        fclose(f);
+        if (!rc)
+            printf("result is %lld\n", res);
+        //
     }
-    //
     return rc;
 }
 
-int usage(void)
+void usage(void)
 {
     printf("example.exe <name file>\n");
-    return ERROR_INPUT_FILE;
 }
 
-int input(FILE*f, int* pa, int* rc)
+void input(FILE*f, int* pa, int* rc, long long int* sum)
 {
+    long long int squre = 1;
     int i = 0;
     //
     for (; (fscanf(f, "%d", pa) == 1); ++pa, ++i)
     {
+
         if (i < MAXLENGTH)
         {
+            squre *= *pa;
+            *sum += squre;
             if (*(pa) < 0)
-            {
-                //printf("%d %d\n", pa, *pa);
                 break;
-            }
         }
         else
         {
-            printf("More than 100 items\n");break;
+            printf("More than 100 items\n");
+            *rc = ERROR_ELEMENTS;
         }
     }
-    //printf("%d %d\n", pa-1, *(pa-1));
-    fclose(f);
     if (!i)
-    {
-        *rc = ERROR;
-        return i;
-    }
-    else
-    {
-        if (i < 100)
-            return i;
-        else
-            return i-1;
-    }
-}
-
-void result(int* pa, int* pb, long long int* sum)
-{
-    *sum = 0;
-    int i = 0;
-    long long int squre = 1;
-    //
-    while(pa <= pb)
-    {
-        //printf("%d",*pa);
-        squre *= *pa;
-        //printf("\n[dbg] %lld\n", squre);
-        *sum += squre;
-        //printf("[dbg2] %lld\n", *sum);
-        pa++;
-    }
+        *rc = ERROR_ELEMENTS;
 }
