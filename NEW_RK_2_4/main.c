7099fdca1642eb431b74f29a9cad1be460229cc9
ZakolesnikM <zakolesnik.m@gmail.com>
2017-06-03 11:09:47 +0300

    NEW_RK2

---
+++ NEW_RK_2_4/main.c
@@ -0,0 +1,101 @@
+#include <stdio.h>
+#define MAX 100
+#define OK 0
+#define ERROR_FOUND_FILE -1
+#define ERROR_LEN_ARRAY -2
+#define ERROR_ELEMENT -3
+#define YES 1
+#define NO 2
+
+void usage(void);
+int input_array(FILE*, int*, int*);
+int process(int*, int);
+
+int main(int argc, char** argv)
+{
+	int rc = OK;
+	if (argc != 2)
+		usage();
+	else
+	{
+		FILE* f;
+		f = fopen(argv[1], "r");
+		if (!f)
+		{
+			printf("File '%s' not found!\n", argv[1]);
+			rc = ERROR_FOUND_FILE;
+		}
+		if (!rc)
+		{
+			int len;
+			int array[MAX];
+			if(!input_array(f, array, &len))
+			{
+				printf("\n");
+				int znach = process(array, len);
+				if (znach == YES)
+					printf("yes");
+				else if (znach == ERROR_ELEMENT)
+					printf("Element error");
+				else
+					printf("no");
+			}
+			else
+			{
+				printf("Error in file!");
+			}
+		}
+	}
+	return rc;
+}
+
+void usage(void)
+{
+	printf("example.exe <name file>\n");
+}
+
+int input_array(FILE* f, int* pa, int* len)
+{
+	fscanf(f, "%d\n", len);
+	if (*len > MAX || *len <= 0)
+		return ERROR_LEN_ARRAY;
+	int i, new_len = 0;
+	
+	i = 0;
+	for (; (i < *len) && 
+		fscanf(f, "%d", (pa+i)) == 1; ++i)
+		new_len++;
+	if(!new_len)
+		return ERROR_LEN_ARRAY;
+	
+	*len = new_len;
+	printf("%d\n", *len);
+	i = 0;
+	for (; (i < *len); ++i)
+		printf("%d ", *(pa+i));
+	return OK;
+}
+
+int process(int* pa, int len)
+{
+	int new_array[len];
+	int i = 0;
+	for (; i < len; ++i)
+		if (*(pa+i) >= len)
+			return ERROR_ELEMENT;
+	i = 0;
+	for (; i < len; ++i)
+		new_array[i] = 0;
+	int* pb = new_array;
+	i = 0; //*pb += 1;
+	while(*(pb+i) < 2)
+	{
+		i = *(pa+i);
+		*(pb+i) += 1;
+	}
+	i = 0;
+	for (; i < len; ++i)
+		if (*(pb+i) == 0)
+			return NO;
+	return YES;
+}
