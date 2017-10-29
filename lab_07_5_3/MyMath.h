1bf1d99aada47a6d199f3c65fc8cb14084559d76
ZakolM <zakolesnik.m@gmail.com>
2017-10-29 23:38:56 +0300

    code style

--- lab_07_5_3/MyMath.h
+++ lab_07_5_3/MyMath.h
@@ -1,9 +1,9 @@
 #ifndef __MyMath__H__
 #define __MyMath__H__
 
-void swap(void* a, void* b, int size);
-int compare_int_and_ch (const void * first_item, const void * second_item);
-int compare_double(const void * first_item, const void * second_item);
-void BubbleSort(void* basic, int count, int size, int(*comp)(const void*,const void*));
+void swap(void *a, void *b, int size);
+int compare_int_and_ch (const void *first_item, const void *second_item);
+int compare_double(const void *first_item, const void *second_item);
+void BubbleSort(void* basic, int count, int size, int(*comp)(const void*, const void*));
 
 #endif //__MyMath__H__
