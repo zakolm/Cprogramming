32c21536481cf39ced04f8dec82e113991d6dce7
ZakolM <zakolesnik.m@gmail.com>
2017-11-08 14:49:39 +0300

    lab08, del while, use goto

--- lab_08_04/main.c
+++ lab_08_04/main.c
@@ -26,75 +26,76 @@ int main(int argc, char **argv)
 		rc = ERROR_ALLOCATE_MEMORY;
 	}
 
-	int count = 0;
-	while (rc == OK && !count)
+	//int count = 0;
+	//while (rc == OK && !count)
+	//{
+	//count++;
+	if (!strcmp(argv[1], "a") || !strcmp(argv[1], "m"))
 	{
-		count++;
-		if (!strcmp(argv[1], "a") || !strcmp(argv[1], "m"))
+		FILE * file1 = fopen(argv[3], "r");
+		if (!file1)
 		{
-			FILE * file1 = fopen(argv[3], "r");
-			if (!file1)
-			{
-				rc = ERROR_FILE;
-				break;
-			}
-			matrix_s *matrix_b = NULL, *new_matrix = NULL;
-			matrix_b = create_matrix_from_file(file1);
-			if (!matrix_b)
-			{
-				rc = ERROR_ALLOCATE_MEMORY;
-				fclose(file1);
-				break;
-			}
-			if (!strcmp(argv[1], "a"))
-			{
-				new_matrix = addition_matrix(matrix, matrix_b);
-			}
-			else
-			{
-				new_matrix = multiply_matrix(matrix, matrix_b);
-			}
-
-			free_matrix(matrix_b);
+			rc = ERROR_FILE;
+			goto stop;//break;
+		}
+		matrix_s *matrix_b = NULL, *new_matrix = NULL;
+		matrix_b = create_matrix_from_file(file1);
+		if (!matrix_b)
+		{
+			rc = ERROR_ALLOCATE_MEMORY;
 			fclose(file1);
-			if (!new_matrix)
-			{
-				rc = ERROR_ALLOCATE_MEMORY;
-				break;
-			}
-			FILE * file_write = fopen(argv[4], "w");
+			goto stop;//break;
+		}
+		if (!strcmp(argv[1], "a"))
+		{
+			new_matrix = addition_matrix(matrix, matrix_b);
+		}
+		else
+		{
+			new_matrix = multiply_matrix(matrix, matrix_b);
+		}
+
+		free_matrix(matrix_b);
+		fclose(file1);
+		if (!new_matrix)
+		{
+			rc = ERROR_ALLOCATE_MEMORY;
+			goto stop;//break;
+		}
+		FILE * file_write = fopen(argv[4], "w");
+		if (!file_write)
+		{
+			rc = ERROR_FILE;
+			free_matrix(new_matrix);
+			goto stop;//break;
+		}
+		print_to_file(file_write, new_matrix);
+		free_matrix(new_matrix);
+		fclose(file_write);
+	}
+	else
+	{
+		double det;
+		int flag = determinant(matrix, &det);
+		if (!flag)
+		{
+			FILE * file_write = fopen(argv[3], "w");
 			if (!file_write)
 			{
 				rc = ERROR_FILE;
-				free_matrix(new_matrix);
-				break;
+				goto stop;//break;
 			}
-			print_to_file(file_write, new_matrix);
-			free_matrix(new_matrix);
+			fprintf(file_write, "%f", det);
 			fclose(file_write);
 		}
 		else
 		{
-			double det;
-			int flag = determinant(matrix, &det);
-			if (!flag)
-			{
-				FILE * file_write = fopen(argv[3], "w");
-				if (!file_write)
-				{
-					rc = ERROR_FILE;
-					break;
-				}
-				fprintf(file_write, "%f", det);
-				fclose(file_write);
-			}
-			else
-			{
-				rc = ERROR_DETERMINANT;
-			}
+			rc = ERROR_DETERMINANT;
 		}
 	}
+	//}
 
+	stop:
 	free_matrix(matrix);	
 	fclose(file);
 	print_error(rc);
