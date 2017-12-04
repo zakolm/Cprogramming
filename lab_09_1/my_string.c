0341259e5180a2eb8cdba3870cf45c4fa86a66b5
ZakolM <zakolesnik.m@gmail.com>
2017-12-04 13:23:07 +0300

    lab09, edit code style

--- lab_09_1/my_string.c
+++ lab_09_1/my_string.c
@@ -1,7 +1,5 @@
 #include "my_string.h"
 
-#include <string.h>
-
 static size_t my_strlen(const char *src)
 {
     size_t len = 0;
@@ -16,47 +14,24 @@ static size_t my_strlen(const char *src)
 
 size_t my_strspn(const char *string1, const char *string2)
 {
-
-    //size_t len1 = my_strlen(string1);
-    //size_t len2 = my_strlen(string2);
-
-    /*for (size_t i = 0; i < len2; ++i)
-        if (string1[0] != string2[i] && i == (len2 - 1))
-            return res;
-        else if (string1[0] == string2[i])
-            break;
-
-    res++;*/
-    //int arr[len1];
-    //size_t res = 0;
-    size_t i,j;
+    size_t i = 0, j = 0;
     for (i = 0; string1[i]; ++i)
     {
         for (j = 0; string2[j]; ++j)
-        {
-            if (string1[i] == string2[j])// && !arr[j])
-            {
+            if (string1[i] == string2[j])
                 break;
-                //arr[j] = 1;
-                //res++;
-            }
-        }
         if (!string2[j])
-        {
             break;
-        }
     }
 
     return i;
-    //return strspn(string1, string2);
 }
 
 static void my_strncpy(char * destination, const char * source, size_t num)
 {
     for (size_t i = 0; i < num; ++i)
-    {
         destination[i] = source[i];
-    }
+
     destination[num] = '\0';
 }
 
