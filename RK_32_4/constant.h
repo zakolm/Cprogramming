7d88d2fb6ea4830c8e9729f73dc94e823cd1a9e9
U-VLAB\user-lab01 <user-lab01@LAB-01-20.vlab.bmstu.ru>
2017-12-13 15:24:31 +0300

    RK_34_4

---
+++ RK_32_4/constant.h
@@ -0,0 +1,15 @@
+#ifndef CONSTANT_H
+#define CONSTANT_H
+
+#include <stdio.h>
+
+#define OK 0
+#define ERROR_PARAMS -1
+#define ERROR_FILE -2
+#define ERROR_MEMORY -3
+#define MAX_NAME 70
+#define MAX_GROUP 10
+
+void print_errors(int error);
+
+#endif // CONSTANT_H
